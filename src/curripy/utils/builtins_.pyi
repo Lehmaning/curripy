c3cbd514dd6a6e5888125555ea2bb715e4073425
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2024-08-06 13:42:05 +0800

    feat(builtins): add more type overloadings

--- src/curripy/utils/builtins_.pyi
+++ src/curripy/utils/builtins_.pyi
@@ -1,16 +1,28 @@
-from typing import (
-    TypeVar,
-    MutableMapping,
-    overload,
-    Mapping,
-)
 from _collections_abc import ValuesView, dict_values
+from typing import Callable, Iterable, Mapping, MutableMapping, TypeVar, overload
+
+from typing_extensions import TypeIs
+
+from ..__generics import ParamType, ParamType1, ParamType2
 from ..utils import curry
 
 KeyType = TypeVar("KeyType")
 ValueType = TypeVar("ValueType")
 
-filter_ = curry(filter)
+@overload
+def filter_(
+    func: None,
+) -> Callable[[Iterable[ParamType | None]], filter[ParamType]]:
+    ...
+@overload
+def filter_(
+    func: Callable[[ParamType2], TypeIs[ParamType1]]
+) -> Callable[[Iterable[ParamType2]], filter[ParamType1]]: ...
+@overload
+def filter_(
+    func: Callable[[ParamType], bool]
+) -> Callable[[Iterable[ParamType]], filter]: ...
+
 map_ = curry(map)
 
 @overload
