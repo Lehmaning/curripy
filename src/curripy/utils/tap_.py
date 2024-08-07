542f5d6ea8908948822e1b68262e2579cac6fcdb
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2024-08-08 07:35:53 +0800

    doc(utils): move docstring from pyi to py

--- src/curripy/utils/tap_.py
+++ src/curripy/utils/tap_.py
@@ -2,6 +2,21 @@ __all__ = ["tap"]
 
 
 def tap(func):
+    """
+    To call the function and return its first argument instead of its result.
+    It is a typing-improved version that is inspired by dry-python/returns.
+
+    See also:
+    - https://github.com/dry-python/returns/blob/66460df43ccda4ec8eeb95507f962e26cbb2c480/returns/functions.py#L53
+    - https://x.com/joelnet/status/1165994838564777985
+
+    Args:
+        func (Callable[Concatenate[ParamType, P], None]): _description_
+
+    Returns:
+        Callable[Concatenate[ParamType, P], ParamType]: _description_
+    """
+
     def caller(arg, **kwargs):
         nonlocal func
         func(arg, **kwargs)
--- src/curripy/utils/tap_.pyi
+++ src/curripy/utils/tap_.pyi
@@ -1,21 +1,7 @@
-from typing import Concatenate, Callable
-from ..__generics import ParamType, P
+from typing import Callable
+from ..__generics import ParamType
 
 def tap(
     func: Callable[[ParamType], None],
 ) -> Callable[[ParamType], ParamType]:
-    """
-    To call the function and return its first argument instead of its result.
-    It is a typing-improved version that is inspired by dry-python/returns.
-
-    See also:
-    - https://github.com/dry-python/returns/blob/66460df43ccda4ec8eeb95507f962e26cbb2c480/returns/functions.py#L53
-    - https://xcancel.com/joelnet/status/1165994838564777985
-
-    Args:
-        func (Callable[Concatenate[ParamType, P], None]): _description_
-
-    Returns:
-        Callable[Concatenate[ParamType, P], ParamType]: _description_
-    """
     ...
