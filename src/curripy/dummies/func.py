c37d7d2a28d0b8014a3ceb7b2d2c6436d1bfbe74
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2024-08-09 12:11:59 +0800

    feat(dummies): add some basic functions

--- src/curripy/dummies/func.py
+++ src/curripy/dummies/func.py
@@ -1,3 +1,38 @@
+from ..utils import curry
+from ..__bootstrap.dummies import def_args_kwargs
+
+__all__ = [
+    "def_kwargs",
+    "def_args",
+    "def_args_kwargs",
+    "return_",
+    "return_true",
+    "return_false",
+    "return_0",
+    "return_1",
+]
+
+
 def def_kwargs(**kwargs): ...
 def def_args(*args): ...
-def def_args_kwargs(*args, **kwargs): ...
+
+
+@curry
+def return_(x, _):
+    return x
+
+
+def return_true(_):
+    return True
+
+
+def return_false(_):
+    return False
+
+
+def return_0(_):
+    return 0
+
+
+def return_1(_):
+    return 1
