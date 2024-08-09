73708a05f1e24f7b136760e8e210c0f5fc3d5984
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2024-08-09 18:46:34 +0800

    chore: remove deprecated modules

--- src/curripy/__deprecated/partial_.pyi
+++
@@ -1,9 +0,0 @@
-from functools import partial as partial_
-
-partial = partial_
-"""
-Notice: this is not the true type of curripy.partial.
-The reason for marking curripy.partial as a type alias to functools.partial is
-it makes curripy.partial able to show real type of the function with support of mypy and lsp,
-without any third-party mypy plugin.
-"""
