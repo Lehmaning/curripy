24d0fc6858bdd2740eb5799bf1f2c64171119f40
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2024-08-19 22:32:59 +0800

    fix(type): disable overloading for map_

--- src/curripy/__bootstrap/builtins_.pyi
+++ src/curripy/__bootstrap/builtins_.pyi
@@ -12,6 +12,8 @@ from typing import (
 
 from typing_extensions import TypeIs
 
+from curripy.utils.curry_ import curry
+
 from ..__generics import (
     ParamT,
     ParamT1,
@@ -32,6 +34,9 @@ __all__ = (
 KeyType = TypeVar("KeyType")
 ValueType = TypeVar("ValueType")
 
+# filter_  = curry(filter)
+map_ = curry(map)
+
 @overload
 def filter_(
     func: Callable[[ParamT1], TypeIs[ParamT2]],
@@ -44,56 +49,57 @@ def filter_(
 def filter_(
     func: None,
 ) -> Callable[[Iterable[ParamT | None]], filter[ParamT]]: ...
-@overload
-def map_(
-    func: Callable[[ParamT1], ReturnT],
-) -> Callable[
-    [Iterable[ParamT1]],
-    map[ReturnT],
-]: ...
-@overload
-def map_(
-    func: Callable[[ParamT1, ParamT2], ReturnT],
-) -> Callable[
-    [Iterable[ParamT1], Iterable[ParamT2]],
-    map[ReturnT],
-]: ...
-@overload
-def map_(
-    func: Callable[[ParamT1, ParamT2, ParamT3], ReturnT],
-) -> Callable[
-    [
-        Iterable[ParamT1],
-        Iterable[ParamT2],
-        Iterable[ParamT3],
-    ],
-    map[ReturnT],
-]: ...
-@overload
-def map_(
-    func: Callable[[ParamT1, ParamT2, ParamT3, ParamT4], ReturnT],
-) -> Callable[
-    [
-        Iterable[ParamT1],
-        Iterable[ParamT2],
-        Iterable[ParamT3],
-        Iterable[ParamT4],
-    ],
-    map[ReturnT],
-]: ...
-@overload
-def map_(
-    func: Callable[[ParamT1, ParamT2, ParamT3, ParamT4, ParamT5], ReturnT],
-) -> Callable[
-    [
-        Iterable[ParamT1],
-        Iterable[ParamT2],
-        Iterable[ParamT3],
-        Iterable[ParamT4],
-        Iterable[ParamT5],
-    ],
-    map[ReturnT],
-]: ...
+
+# @overload
+# def map_(
+#     func: Callable[[ParamT1], ReturnT],
+# ) -> Callable[
+#     [Iterable[ParamT1]],
+#     map[ReturnT],
+# ]: ...
+# @overload
+# def map_(
+#     func: Callable[[ParamT1, ParamT2], ReturnT],
+# ) -> Callable[
+#     [Iterable[ParamT1], Iterable[ParamT2]],
+#     map[ReturnT],
+# ]: ...
+# @overload
+# def map_(
+#     func: Callable[[ParamT1, ParamT2, ParamT3], ReturnT],
+# ) -> Callable[
+#     [
+#         Iterable[ParamT1],
+#         Iterable[ParamT2],
+#         Iterable[ParamT3],
+#     ],
+#     map[ReturnT],
+# ]: ...
+# @overload
+# def map_(
+#     func: Callable[[ParamT1, ParamT2, ParamT3, ParamT4], ReturnT],
+# ) -> Callable[
+#     [
+#         Iterable[ParamT1],
+#         Iterable[ParamT2],
+#         Iterable[ParamT3],
+#         Iterable[ParamT4],
+#     ],
+#     map[ReturnT],
+# ]: ...
+# @overload
+# def map_(
+#     func: Callable[[ParamT1, ParamT2, ParamT3, ParamT4, ParamT5], ReturnT],
+# ) -> Callable[
+#     [
+#         Iterable[ParamT1],
+#         Iterable[ParamT2],
+#         Iterable[ParamT3],
+#         Iterable[ParamT4],
+#         Iterable[ParamT5],
+#     ],
+#     map[ReturnT],
+# ]: ...
 def values(d: Mapping[KeyType, ValueType]) -> ValuesView[ValueType]: ...
 def items(d: Mapping[KeyType, ValueType]) -> ItemsView[KeyType, ValueType]: ...
 def keys(d: Mapping[KeyType, ValueType]) -> KeysView[KeyType]: ...
