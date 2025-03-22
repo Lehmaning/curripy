500a7a1042a5184d04eba5c092d5d318085fe8f6
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2025-03-23 00:45:41 +0800

    chore(compose): use PS as params' types for 1st function

--- src/curripy/__bootstrap/compose_.pyi
+++ src/curripy/__bootstrap/compose_.pyi
@@ -2,7 +2,6 @@ from typing import Callable, overload
 
 from ..__generics import (
     ArgKwargP,
-    ParamT,
     ReturnT,
     ReturnT1,
     ReturnT2,
@@ -20,17 +19,17 @@ __all__ = (
 )
 
 def dot(
-    f: Callable[[ParamT], ReturnT1], g: Callable[[ReturnT1], ReturnT2]
-) -> Callable[[ParamT], ReturnT2]:
+    f: Callable[ArgKwargP, ReturnT1], g: Callable[[ReturnT1], ReturnT2]
+) -> Callable[ArgKwargP, ReturnT2]:
     """
     A function that acts lke '.' in Haskell, which does not mean the dot operator between matrices.
 
     Args:
-        f (Callable[[ParamT], ReturnT1]): first function
+        f (Callable[ArgKwargP, ReturnT1]): first function
         g (Callable[[ReturnT1], ReturnT2]): second function
 
     Returns:
-        Callable[[ParamT], ReturnT2]: composed function
+        Callable[ArgKwargP, ReturnT2]: composed function
     """
     ...
 
@@ -43,30 +42,30 @@ def pipe(
 ) -> Callable[ArgKwargP, ReturnT1]: ...
 @overload
 def pipe(
-    func1: Callable[[ParamT], ReturnT1],
+    func1: Callable[ArgKwargP, ReturnT1],
     func2: Callable[[ReturnT1], ReturnT2],
-) -> Callable[[ParamT], ReturnT2]: ...
+) -> Callable[ArgKwargP, ReturnT2]: ...
 @overload
 def pipe(
-    func1: Callable[[ParamT], ReturnT1],
+    func1: Callable[ArgKwargP, ReturnT1],
     func2: Callable[[ReturnT1], ReturnT2],
     func3: Callable[[ReturnT2], ReturnT3],
-) -> Callable[[ParamT], ReturnT3]: ...
+) -> Callable[ArgKwargP, ReturnT3]: ...
 @overload
 def pipe(
-    func1: Callable[[ParamT], ReturnT1],
+    func1: Callable[ArgKwargP, ReturnT1],
     func2: Callable[[ReturnT1], ReturnT2],
     func3: Callable[[ReturnT2], ReturnT3],
     func4: Callable[[ReturnT3], ReturnT4],
-) -> Callable[[ParamT], ReturnT4]: ...
+) -> Callable[ArgKwargP, ReturnT4]: ...
 @overload
 def pipe(
-    func1: Callable[[ParamT], ReturnT1],
+    func1: Callable[ArgKwargP, ReturnT1],
     func2: Callable[[ReturnT1], ReturnT2],
     func3: Callable[[ReturnT2], ReturnT3],
     func4: Callable[[ReturnT3], ReturnT4],
     func5: Callable[[ReturnT4], ReturnT5],
-) -> Callable[[ParamT], ReturnT5]: ...
+) -> Callable[ArgKwargP, ReturnT5]: ...
 @overload
 def compose(
     func1: Callable[ArgKwargP, ReturnT],
@@ -74,26 +73,26 @@ def compose(
 @overload
 def compose(
     func2: Callable[[ReturnT1], ReturnT2],
-    func1: Callable[[ParamT], ReturnT1],
-) -> Callable[[ParamT], ReturnT1]: ...
+    func1: Callable[ArgKwargP, ReturnT1],
+) -> Callable[ArgKwargP, ReturnT1]: ...
 @overload
 def compose(
     func3: Callable[[ReturnT2], ReturnT3],
     func2: Callable[[ReturnT1], ReturnT2],
-    func1: Callable[[ParamT], ReturnT1],
-) -> Callable[[ParamT], ReturnT3]: ...
+    func1: Callable[ArgKwargP, ReturnT1],
+) -> Callable[ArgKwargP, ReturnT3]: ...
 @overload
 def compose(
     func4: Callable[[ReturnT3], ReturnT4],
     func3: Callable[[ReturnT2], ReturnT3],
     func2: Callable[[ReturnT1], ReturnT2],
-    func1: Callable[[ParamT], ReturnT1],
-) -> Callable[[ParamT], ReturnT4]: ...
+    func1: Callable[ArgKwargP, ReturnT1],
+) -> Callable[ArgKwargP, ReturnT4]: ...
 @overload
 def compose(
     func5: Callable[[ReturnT4], ReturnT5],
     func4: Callable[[ReturnT3], ReturnT4],
     func3: Callable[[ReturnT2], ReturnT3],
     func2: Callable[[ReturnT1], ReturnT2],
-    func1: Callable[[ParamT], ReturnT1],
-) -> Callable[[ParamT], ReturnT5]: ...
+    func1: Callable[ArgKwargP, ReturnT1],
+) -> Callable[ArgKwargP, ReturnT5]: ...
